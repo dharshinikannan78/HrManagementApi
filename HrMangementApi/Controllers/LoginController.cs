8f8bcc74ab7e3f6fef40efc3e9f510bb0679b8e7
dharshinikannan78 <dharshinikannan78@gmail.com>
2022-07-25 16:35:22 +0530

    commit

---
+++ HrMangementApi/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using HrMangementApi.Model;
+using HrMangementApi.UserDbContext;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HrMangementApi.Controllers
+{
+    [EnableCors("AllowOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly UserdbContext dataContext;
+        public LoginController(UserdbContext _dataContext)
+        {
+            dataContext = _dataContext;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult GetLogin([FromBody] Login loginData)
+        {
+            var user = dataContext.LoginModels.Where(q => q.UserName == loginData.UserName && q.Password == loginData.Password);
+            return Ok(user);
+        }
+
+        [HttpPost("AddUser")]
+        public IActionResult AddUserlogin([FromBody] Login loginData)
+        {
+
+            dataContext.LoginModels.Add(loginData);
+            dataContext.SaveChanges();
+            return Ok(loginData);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult DeletUser(int id)
+        {
+            var delete = dataContext.LoginModels.Find(id);
+            if (delete == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                dataContext.LoginModels.Remove(delete);
+                dataContext.SaveChanges();
+                return Ok();
+            }
+        }
+
+    }
+}
---
+++ HrMangementApi/UserDbContext/UserdbContext.cs
@@ -0,0 +1,19 @@
+﻿using HrMangementApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HrMangementApi.UserDbContext
+{
+    public class UserdbContext : DbContext
+    {
+        public UserdbContext(DbContextOptions<UserdbContext> Options) : base(Options)
+        {
+        }
+        public DbSet<Login> LoginModels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBulider)
+
+        {
+            modelBulider.Entity<Login>().ToTable("login");
+        }
+    }
+}
