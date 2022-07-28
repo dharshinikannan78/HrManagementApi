fa596783e5c6cb54874f58c25e438619ff3295b2
salman <mohamedsalmankhan509>
2022-07-28 15:55:09 +0530

    july 28

---
+++ HrMangementApi/Controllers/LeaveController.cs
@@ -0,0 +1,63 @@
+﻿using HrMangementApi.Model;
+using HrMangementApi.UserDbContext;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace HrMangementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class LeaveController : ControllerBase
+    {
+        private readonly UserdbContext dataContext;
+        public LeaveController(UserdbContext _dataContext)
+        {
+            dataContext = _dataContext;
+        }
+        [HttpPost("ApplyLeave")]
+        public IActionResult ApplyLeave([FromBody] LeaveDetails LeaveData)
+        {
+            dataContext.LeaveModel.Add(LeaveData);
+            dataContext.SaveChanges();
+            return Ok(LeaveData);
+        }
+        [HttpGet("GetAllLeaveDetails")]
+        public IActionResult AllLeaveDetails()
+        {
+            var LeaveDetails = dataContext.LeaveModel.AsQueryable();
+            return Ok(LeaveDetails);
+        }
+
+        [HttpGet("GetUserLeaveDetails")]
+        public IActionResult UserLeaveDetails(int LeaveData)
+        {
+            var LeaveDetails = dataContext.LeaveModel.Where(a => a.EmployeeId == LeaveData);
+            if (LeaveDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(LeaveDetails);
+        }
+
+        [HttpPut("UpdateLeaveDetails")]
+        public IActionResult UpdateLeaveDetails([FromBody] LeaveDetails LeaveData)
+        {
+            var res = dataContext.LeaveModel.AsNoTracking().FirstOrDefault(a => a.LeaveId == LeaveData.LeaveId);
+            if (res == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                dataContext.Entry(LeaveData).State = EntityState.Modified;
+                dataContext.SaveChanges();
+                return Ok();
+            }
+        }
+
+    }
+}
