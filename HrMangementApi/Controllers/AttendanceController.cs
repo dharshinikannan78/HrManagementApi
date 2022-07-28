3d59e5d1412d994d33c2f488457733cabae202c1
salman <mohamedsalmankhan509>
2022-07-28 16:11:09 +0530

    attnd controller

---
+++ HrMangementApi/Controllers/AttendanceController.cs
@@ -0,0 +1,32 @@
+﻿using HrMangementApi.Model;
+using HrMangementApi.UserDbContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HrMangementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly UserdbContext dataContext;
+        public AttendanceController(UserdbContext _dataContext)
+        {
+            dataContext = _dataContext;
+        }
+
+        [HttpGet("AllAttendance")]
+        public IActionResult AllAttendance()
+        {
+            var details = dataContext.AttendanceModel.AsQueryable();
+            return Ok(details);
+        }
+        [HttpPost("AddAttendance")]
+        public IActionResult AddAttendance([FromBody] AttendanceDetails Data)
+        {
+            dataContext.AttendanceModel.Add(Data);
+            dataContext.SaveChanges();
+            return Ok(Data);
+        }
+    }
+}
